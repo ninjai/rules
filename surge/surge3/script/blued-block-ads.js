51bd5d0efc6cad186d4137b354350f331bc65ee1
ninjai <362778+ninjai@users.noreply.github.com>
2020-04-17 01:20:54 +0800

    Blued block ads

---
+++ surge/surge3/script/blued-block-ads.js
@@ -0,0 +1,15 @@
+if (typeof $response != "undefined") {
+  let obj = JSON.parse($response.body)
+  if (obj.data) {
+    if ($request.url.includes('splash')) {
+      obj.data = []
+    }
+    else if ($request.url.includes('users')) {
+      obj.data = obj.data.filter(item => item.name != "")
+    }
+  }
+  $done({ body: JSON.stringify(obj) })
+}
+else {
+  $done({})
+}
