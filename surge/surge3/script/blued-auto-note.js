230550592d02f5f64ce0342cc32ae1481aca5c66
ninjai <362778+ninjai@users.noreply.github.com>
2020-04-18 11:45:07 +0800

    Give up...

--- surge/surge3/script/blued-auto-note.js
+++ surge/surge3/script/blued-auto-note.js
@@ -1,30 +1,34 @@
-const headers = JSON.parse($persistentStore.read('headersBlued'))
-const obj = JSON.parse($response.body)
-const id = obj.data[0].uid
-const name = obj.data[0].name
-
-if ($persistentStore.read('IdBlued')) {
-  const options = {
-    'url': `https://argo.blued.cn/users/${$persistentStore.read('IdBlued')}/notes`,
-    'headers': headers,
-    'body': {
-      'uid': id,
-      'note': '拒-丑'
-    }
-  }
+if (typeof $response != 'undefined') {
+  const obj = JSON.parse($response.body)
 
   if (obj.data[0].note === '') {
-    $httpClient.post(options, function(error, response, data) {
-      if (error) {
-        $notification.post('备注失败', name, error)
-      } else {
-        $notification.post('备注成功', '', name)
+    if ($persistentStore.read('IdBlued')) {
+      const id = obj.data[0].uid
+      const name = obj.data[0].name
+      const headers = JSON.parse($persistentStore.read('headersBlued'))
+
+      const options = {
+        'url': `https://argo.blued.cn/users/${$persistentStore.read('IdBlued')}/notes`,
+        'headers': headers,
+        'body': {
+          'uid': id,
+          'note': '拒-丑'
+        }
       }
-    })
+
+      $httpClient.post(options, function(error, response, data) {
+        if (error) {
+          $notification.post('备注失败', name, error)
+        } else {
+          $notification.post('备注成功', '', name)
+        }
+      })
+    }
+    else {
+      $notification.post('请先获取ID', '', '')
+    }
   }
 }
-else {
-  $notification.post('请先获取ID', '', '')
-}
 
-$done({})
+
+$done({})  
