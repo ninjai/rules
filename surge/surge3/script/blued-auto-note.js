6af9b04ca1f040c809c03b95b217eef8aeea16f3
ninjai <362778+ninjai@users.noreply.github.com>
2020-04-19 14:50:48 +0800

    Blued view count debug

--- surge/surge3/script/blued-auto-note.js
+++ surge/surge3/script/blued-auto-note.js
@@ -41,7 +41,7 @@ if (typeof $response != 'undefined') {
     let obj = JSON.parse($persistentStore.read('ViewCountBlued'))
     obj = { ...obj, ...{ 'count': ++obj.count } }
     $persistentStore.write(JSON.stringify(obj), 'ViewCountBlued')
-    if (count % 100 === 0) {
+    if (obj.count % 100 === 0) {
       $notification.post('累计浏览人数', '', `自${obj.startTime}以来共浏览了${obj.count}人`)
     }
   }
