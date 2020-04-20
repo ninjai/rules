a3e26bdcb87cbb00ff59a58006c1e58e6b884c4f
ninjai <362778+ninjai@users.noreply.github.com>
2020-04-20 16:35:57 +0800

    Blued remove list ads

--- surge/surge3/script/blued-filter-list.js
+++ surge/surge3/script/blued-filter-list.js
@@ -2,26 +2,28 @@ function filterList(data) {
   return data.filter(item => item.note === '' && item.is_invisible_half === 0)
 }
 
-function listGetMore(start) {
-  let data = []
-  let url = $request.url.match(/(^https:\/\/)([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(\/users\?filters.*)start\=(\d+)(.*$)/)
-  start = start + Number.parseInt(url[2], 10)
-  url = `${url[1]}argo.blued.cn${url[3]}start=${start}${url[5]}`
-  return () => $httpClient.get({ url }, (error, response, data) => {
-    if (error) {
-      $notification.post('获取更多失败', '', error)
-      console.log(url)
-      return []
-    } else {
-      let obj = JSON.parse(data)
-      if (obj.data) {
-        $notification.post('获取更多成功', '', '')
-        return filterList(data)
-      }
-      else {
+const listGetMore = start => {
+  return new Promise(resolve => {
+    let data = []
+    let url = $request.url.match(/(^https:\/\/)([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(\/users\?filters.*)start\=(\d+)(.*$)/)
+    start = start + Number.parseInt(url[2], 10)
+    url = `${url[1]}argo.blued.cn${url[3]}start=${start}${url[5]}`
+    $httpClient.get({ url }, (error, response, data) => {
+      if (error) {
+        $notification.post('获取更多失败', '', error)
+        console.log(url)
         return []
+      } else {
+        let obj = JSON.parse(data)
+        if (obj.data) {
+          $notification.post('获取更多成功', '', '')
+          return filterList(data)
+        }
+        else {
+          return []
+        }
       }
-    }
+    })
   })
 }
 
