ed93119065ff998813191b268eb76c8722d17b37
ninjai <362778+ninjai@users.noreply.github.com>
2020-04-20 15:17:50 +0800

    Blued remove list ads

--- surge/surge3/script/blued-filter-list.js
+++ surge/surge3/script/blued-filter-list.js
@@ -1,8 +1,5 @@
 function filterList(data) {
-  let data = data.filter(item => item.note === '')
-  data.filter(item => item.is_invisible_half == 0)
-
-  return data
+  return data.filter(item => item.note === '' && item.is_invisible_half === 0)
 }
 
 function listGetMore(start) {
