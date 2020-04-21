e29b28e56e36092a7114285c9a0a28e6e156a62b
ninjai <362778+ninjai@users.noreply.github.com>
2020-04-21 17:17:05 +0800

    Blued list load more

--- surge/surge3/script/blued-filter-list.js
+++ surge/surge3/script/blued-filter-list.js
@@ -2,13 +2,13 @@ function filterList(data) {
   return data.filter(item => item.note === '' && item.is_invisible_half === 0)
 }
 
-const listGetMore = start => {
+async function listGetMore(start) {
   let url = $request.url.match(/(^https:\/\/)([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(\/users\?filters.*)start\=(\d+)(.*$)/)
   start = start + Number.parseInt(url[2], 10)
   url = `${url[1]}argo.blued.cn${url[3]}start=${start}${url[5]}`
   const headers = JSON.parse($persistentStore.read('headersBlued'))
 
-  $httpClient.get({ url, headers }, (error, response, data) => {
+  return await $httpClient.get({ url, headers }, (error, response, data) => {
     if (error) {
       $notification.post('获取更多失败', '', error)
     } else {
@@ -26,9 +26,11 @@ if (typeof $response != 'undefined') {
   let obj = JSON.parse($response.body)
   if (obj.data) {
     obj.data = filterList(obj.data)
-    // while (obj.data.length < 10) {
-    //   obj.data = [...obj.data, ...await listGetMore(60 * n)]
-    // }
+    let n = 1
+    while (obj.data.length < 10) {
+      obj.data = [...obj.data, ...listGetMore(60 * n)]
+      n++
+    }
   }
   if (obj.extra) {
     if (obj.extra.adms) {
