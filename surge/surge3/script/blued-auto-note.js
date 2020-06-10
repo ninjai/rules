ea8b338685f71def1b1f65320f0ee96b7fedc902
ninjai <362778+ninjai@users.noreply.github.com>
2020-06-10 16:49:24 +0800

    add some profile modifications

--- surge/surge3/script/blued-auto-note.js
+++ surge/surge3/script/blued-auto-note.js
@@ -5,6 +5,14 @@ if (typeof $response != 'undefined') {
     obj.data[0].ads_banner.length = 0
   }
 
+  obj.data[0].access_private_photos = 1
+  obj.data[0].allow_show_reports = 1
+  obj.data[0].is_access_follows = 1
+  obj.data[0].is_access_groups = 1
+  obj.data[0].is_locked = 0
+  obj.data[0].aaid = ''
+  obj.data[0].mate = 0
+
   if (obj.data[0].note === '') {
     if ($persistentStore.read('IdBlued')) {
 
