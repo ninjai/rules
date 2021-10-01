3f20392d965fbf04f568f32f1ba29420e24149e6
ninjai <362778+ninjai@users.noreply.github.com>
2021-10-01 18:27:04 +0800

    fix

--- surge/surge3/script/network-info-panel.js
+++ surge/surge3/script/network-info-panel.js
@@ -6,8 +6,8 @@ let v6 = $network.v6.primaryAddress
 let routerv6 = $network.v6.primaryRouter
 
 $done({
-    "title": "IP Information",
-    "content": "无线网络: ${ssid}\nIPv4: ${IPv4}\nIPv6: ${IPv6}\nIPv4网关: ${routerv4}\nIPv6网关: ${routerv6}",
+    "title": "网络信息",
+    "content": `无线网络: ${ssid}\nIPv4: ${IPv4}\nIPv6: ${IPv6}\nIPv4网关: ${routerv4}\nIPv6网关: ${routerv6}`,
     "icon": "wifi",
     "icon-color": "#0EEA3B",
  })
