8a0eef2a66860bb9a96f5d54020156ea2ff0b9ed
ninjai <362778+ninjai@users.noreply.github.com>
2021-10-01 18:54:28 +0800

    add logic

--- surge/surge3/script/network-info-panel.js
+++ surge/surge3/script/network-info-panel.js
@@ -1,13 +1,24 @@
+let bssid = $network.wifi.bssid
 let ssid = $network.wifi.ssid
+let carrier = $network["cellular-data"].carrier
 let radio = $network["cellular-data"].radio
 let v4 = $network.v4.primaryAddress
 let routerv4 = $network.v4.primaryRouter
 let v6 = $network.v6.primaryAddress
-let routerv6 = $network.v6.primaryRouter
 
-$done({
-    "title": "网络信息",
-    "content": `无线网络: ${ssid}\nIPv4: ${v4}\nIPv6: ${v6}\nIPv4网关: ${routerv4}\nIPv6网关: ${routerv6}`,
-    "icon": "wifi",
-    "icon-color": "#0EEA3B",
- })
+if(ssid!=='undefined'){
+    $done({
+        "title": "网络信息",
+        "content": `无线网络: ${ssid}\nIPv4: ${v4}\nIPv6: ${v6}\nIPv4网关: ${routerv4}`,
+        "icon": "wifi",
+        "icon-color": "#0EEA3B",
+    })
+}
+else{
+    $done({
+        "title": "网络信息",
+        "content": `移动网络: ${carrier},${radio}\nIPv4: ${v4}\nIPv6: ${v6}\nIPv4网关: ${routerv4}`,
+        "icon": "simcard.fill",
+        "icon-color": "#EA0300",
+    })
+}
