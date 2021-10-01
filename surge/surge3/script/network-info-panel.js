7492f91df35df6652ba3965d46372ba8252f3ed4
ninjai <362778+ninjai@users.noreply.github.com>
2021-10-02 00:08:23 +0800

    add null condition

--- surge/surge3/script/network-info-panel.js
+++ surge/surge3/script/network-info-panel.js
@@ -2,11 +2,11 @@ let bssid = $network.wifi.bssid
 let ssid = $network.wifi.ssid
 let carrier = $network["cellular-data"].carrier
 let radio = $network["cellular-data"].radio
-let v4 = $network.v4.primaryAddress
-let routerv4 = $network.v4.primaryRouter
-let v6 = $network.v6.primaryAddress
+let v4 = $network.v4.primaryAddress === null ? "无法获取" : $network.v4.primaryAddress
+let routerv4 = $network.v4.primaryRouter === null ? "无法获取" : $network.v4.primaryRouter
+let v6 = $network.v6.primaryAddress === null ? "无法获取" : $network.v6.primaryAddress
 
-if(ssid!==null){
+if (ssid !== null) {
     $done({
         "title": "网络信息",
         "content": `无线网络: ${ssid}\nIPv4: ${v4}\nIPv4网关: ${routerv4}\nIPv6: ${v6}`,
@@ -14,7 +14,7 @@ if(ssid!==null){
         "icon-color": "#0EEA3B",
     })
 }
-else{
+else {
     $done({
         "title": "网络信息",
         "content": `移动网络: ${carrier},${radio}\nIPv4: ${v4}\nIPv4网关: ${routerv4}\nIPv6: ${v6}`,
